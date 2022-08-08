c781d1042df43844011f8c747bd8667498010617
FranRatias <110835376+FranRatias@users.noreply.github.com>
2022-08-08 18:28:47 +0200

    Update Ejercicio2.py

--- Tema4/Ejercicio2.py
+++ Tema4/Ejercicio2.py
@@ -2,6 +2,9 @@ print("Vamos a indicar los número impares entre dos números")
 inicio = int(input("Indica el número inicio: "))
 fin = int(input("Indica un número fin: "))
 numero = inicio
+lista=[]
 for numero in range(inicio, fin):
     if numero % 2 != 0:
-        print("El número", numero, "es impar")
+        lista.append(numero)
+
+print(lista)
